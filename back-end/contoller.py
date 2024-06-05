b6488c50b3ecff6fd5185f0077d71727b9fa09bc
Mezz <mezzourhyassine@icloud.com>
2024-06-05 02:42:22 +0100

    Ajout des biblio necessaire dans le controller

--- back-end/contoller.py
+++ back-end/contoller.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from typing import Union
+from models import User,Diabete
+from flask import Flask, request, render_template, session, redirect, url_for, send_file
+from typing import Union
+import smtplib
+import matplotlib
+import numpy
+from matplotlib import pyplot as plt 
+from dal import *
+from service import *
+from servplot import *
+import os
+
+from fpdf import FPDF
+matplotlib.use('agg') 
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello World from Backend!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.secret_key = "$T@2023"
